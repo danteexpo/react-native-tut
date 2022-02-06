8bbf4415f8651b89f378454f2fee7c49b32cbc86
dann3223 <81563241+dann3223@users.noreply.github.com>
2022-02-06 10:18:08 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Text, View, StyleSheet, Image, TouchableOpacity, Alert } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Hello World!!!</Text>
+      <Image
+        source={{uri:'https://picsum.photos/200/200'}}
+        style={styles.image}
+      />
+      <TouchableOpacity  
+        style={styles.touchable}
+        onPress={() => Alert.alert('Hello!!!')}
+      >
+        <Text style={styles.title}>Touchable</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    justifyContent: 'space-around',
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#121212',
   },
-});
+  title: {
+    fontSize: 30,
+    fontWeight: '800',
+    color: '#FFF',
+  },
+  image: {
+    height: 200, 
+    width: 200
+  },
+  touchable: {
+    backgroundColor: '#1A69ED',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 5,
+  },
+})
+
+export default App;
